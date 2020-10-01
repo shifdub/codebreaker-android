6cf7224970b8f287fea97062681c31249c011a4a
isaac <shifdub@gmail.com>
2020-10-01 15:06:37 -0600

    Building custom adapter for guesses.

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/adapter/GuessAdapter.java
@@ -0,0 +1,50 @@
+package edu.cnm.deepdive.codebreaker.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.model.Code.Guess;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GuessAdapter extends ArrayAdapter<Guess> {
+  private static final char[] colorCharacters = {'R','O', 'Y', 'B', 'V'};
+  private static final int[] colorValues =
+      {Color.RED, 0xffa500, Color.YELLOW, Color.GREEN, Color.BLUE, 0xee82ee};
+
+  private final Map<Character, Integer> colorMap;
+
+
+  public GuessAdapter(@NonNull Context context) {
+    super(context, R.layout.item_guess, new ArrayList<Guess>());
+    colorMap = new HashMap<>();
+    for (int i = 0; i < colorCharacters.length; i++){
+      colorMap.put(colorCharacters[i], colorValues[i]);
+    }
+  }
+
+  @NonNull
+  @Override
+  public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    View layout = (convertView != null)
+        ? convertView
+        : LayoutInflater.from(getContext()).inflate(R.layout.item_guess, parent, false);
+      Guess guess = getItem(position);
+      TextView correct = layout.findViewById(R.id.correct);
+      TextView close = layout.findViewById(R.id.close);
+      correct.setText(String.valueOf(guess.getCorrect()));
+      close.setText(Integer.toString(guess.getClose()));
+      // TODO Iterate over the characters of guess.getText(), and create ImageView with corresponding background color.
+
+    return layout;
+  }
+
+}
--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -7,50 +9,71 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.adapter.GuessAdapter;
 import edu.cnm.deepdive.codebreaker.model.Code.Guess;
 import edu.cnm.deepdive.codebreaker.model.Game;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements  OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
-  private static final String POOL ="ROYGBIV";
+  private static final String POOL = "ROYGBIV";
   private static final int CODE_LENGTH = 4;
 
-  private ListView guessList = findViewById(R.id.guess_list);
+  private ListView guessList;
   private EditText guess;
   private Button submit;
   private Game game;
-  private ArrayAdapter<Guess> adapter;
+  private GuessAdapter adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    guessList = findViewById(R.id.guess_list);
     guess = findViewById(R.id.guess);
     submit = findViewById(R.id.submit);
     submit.setOnClickListener(this);
     game = new Game(POOL, CODE_LENGTH, new SecureRandom());
-    adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<Guess>());
+    adapter = new GuessAdapter(this);
     guessList.setAdapter(adapter);
+  }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.new_game:
+        // TODO Start new game.
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
   }
 
   @Override
   public void onClick(View v) {
     try {
-      String text = guess.getText().toString().toUpperCase();
+      String text = this.guess.getText().toString().toUpperCase();
       Guess guess = game.guess(text);
       adapter.add(guess);
+      guessList.setSelection(adapter.getCount() - 1);
       this.guess.setText("");
     } catch (IllegalArgumentException e) {
-      Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+      Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
     }
-
   }
-
 
 }
