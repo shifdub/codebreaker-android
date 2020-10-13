d59cb1a73a1e65b706df61ef7418eb6dd9ef7c52
isaac <shifdub@gmail.com>
2020-10-12 22:11:04 -0600

    fixed after computer crashed hopefully up to date

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -1,6 +1,5 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputFilter;
@@ -11,7 +10,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -25,7 +23,7 @@ import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GameFragment extends Fragment implements InputFilter{
+public class GameFragment extends Fragment implements InputFilter {
 
   private static final String INVALID_CHAR_PATTERN = String.format("[^%s]", MainViewModel.POOL);
   private static final int[] colorValues =
@@ -43,6 +41,7 @@ public class GameFragment extends Fragment implements InputFilter{
     super.onCreate(savedInstanceState);
     setHasOptionsMenu(true);
   }
+
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -52,44 +51,14 @@ public class GameFragment extends Fragment implements InputFilter{
     return binding.getRoot();
   }
 
-  private void setupViewModel() {
-    FragmentActivity activity = getActivity();
-    //noinspection ConstantConditions
-    adapter = new GuessAdapter(activity, colorMap);
-
-    viewModel = new ViewModelProvider(activity).get(MainViewModel.class);
-    LifecycleOwner lifecycleOwner = getViewLifecycleOwner();
-    viewModel.getGame().observe(lifecycleOwner, (game) -> {
-      adapter.clear();
-      adapter.addAll(game.getGuesses());
-      binding.guessList.setAdapter(adapter);
-      binding.guessList.setSelection(adapter.getCount() - 1);
-      codeLength = game.getLength();
-      binding.guess.setText("");
-    });
-    viewModel.getSolved().observe(lifecycleOwner, solved ->
-        binding.guessControls.setVisibility(solved ? View.INVISIBLE :View.VISIBLE));
-    viewModel.getThrowable().observe(lifecycleOwner, (throwable)-> {
-      if(throwable != null) {
-        Toast.makeText(activity, throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-      }
-    });
-  }
-
-  private void setupViews() {
-    binding.guess.setFilters(new InputFilter[]{this});
-    binding.submit.setOnClickListener((view) -> recordGuess());
-
-  }
-
   @Override
-  public void onViewCreated(@NonNull  View view, @Nullable Bundle savedInstanceState) {
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     setupViewModel();
   }
 
   @Override
-  public void onCreateOptionsMenu(@NonNull Menu menu,@NonNull MenuInflater inflater) {
+  public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
     super.onCreateOptionsMenu(menu, inflater);
     inflater.inflate(R.menu.game_options, menu);
   }
@@ -104,10 +73,48 @@ public class GameFragment extends Fragment implements InputFilter{
       case R.id.restart_game:
         restartGame();
         break;
-        default:
+      default:
         handled = super.onOptionsItemSelected(item);
     }
     return handled;
+  }
+
+  @Override
+  public CharSequence filter(CharSequence source, int sourceStart, int sourceEnd,
+      Spanned dest, int destStart, int destEnd) {
+    String modifiedSource = source.toString().toUpperCase().replaceAll(INVALID_CHAR_PATTERN, "");
+    StringBuilder builder = new StringBuilder(dest);
+    builder.replace(destStart, destEnd, modifiedSource);
+    if (builder.length() > codeLength) {
+      modifiedSource =
+          modifiedSource.substring(0, modifiedSource.length() - (builder.length() - codeLength));
+    }
+    int newLength = dest.length() - (destEnd - destStart) + modifiedSource.length();
+    binding.submit.setEnabled(newLength == codeLength);
+    return modifiedSource;
+  }
+
+  private void setupViews() {
+    binding.guess.setFilters(new InputFilter[]{this});
+    binding.submit.setOnClickListener((view) -> recordGuess());
+  }
+
+  private void setupViewModel() {
+    FragmentActivity activity = getActivity();
+    //noinspection ConstantConditions
+    adapter = new GuessAdapter(activity, colorMap);
+    viewModel = new ViewModelProvider(activity).get(MainViewModel.class);
+    LifecycleOwner lifecycleOwner = getViewLifecycleOwner();
+    viewModel.getGame().observe(lifecycleOwner, (game) -> {
+      adapter.clear();
+      adapter.addAll(game.getGuesses());
+      binding.guessList.setAdapter(adapter);
+      binding.guessList.setSelection(adapter.getCount() - 1);
+      codeLength = game.getLength();
+      binding.guess.setText("");
+    });
+    viewModel.getSolved().observe(lifecycleOwner, solved ->
+        binding.guessControls.setVisibility(solved ? View.INVISIBLE : View.VISIBLE));
   }
 
   private void recordGuess() {
@@ -128,21 +135,6 @@ public class GameFragment extends Fragment implements InputFilter{
       colorMap.put(chars[i], values[i]);
     }
     return colorMap;
-  }
-
-  @Override
-  public CharSequence filter(CharSequence source, int sourceStart, int sourceEnd,
-      Spanned dest, int destStart, int destEnd) {
-    String modifiedSource = source.toString().toUpperCase().replaceAll(INVALID_CHAR_PATTERN, "");
-    StringBuilder builder = new StringBuilder(dest);
-    builder.replace(destStart, destEnd, modifiedSource);
-    if (builder.length() > codeLength) {
-      modifiedSource =
-          modifiedSource.substring(0, modifiedSource.length() - (builder.length() - codeLength));
-    }
-    int newLength = dest.length() - (destEnd - destStart) + modifiedSource.length();
-    binding.submit.setEnabled(newLength == codeLength);
-    return modifiedSource;
   }
 
 }
