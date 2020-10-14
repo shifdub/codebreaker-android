fb23087650523ae3bc2ccbc6694cf82cf9ab3d27
isaac <shifdub@gmail.com>
2020-10-14 11:54:27 -0600

    database implemented with basic funtionality

--- app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
@@ -2,12 +2,20 @@ package edu.cnm.deepdive.codebreaker;
 
 import android.app.Application;
 import android.util.Log;
+import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.codebreaker.service.CodeBreakerDatabase;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
 
 public class CodebreakerApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
-    Log.d (getClass().getName(),"Testing logging");
+    Stetho.initializeWithDefaults(this);
+    CodeBreakerDatabase.setContext(this);
+    CodeBreakerDatabase.getInstance().getGameDao().delete()
+        .subscribeOn(Schedulers.io())
+        .subscribe();
   }
 }
---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodeBreakerDatabase.java
@@ -0,0 +1,51 @@
+package edu.cnm.deepdive.codebreaker.service;
+
+import android.app.Application;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+import edu.cnm.deepdive.codebreaker.GameDao.model.doa.GameDao;
+import edu.cnm.deepdive.codebreaker.model.entity.Game;
+import edu.cnm.deepdive.codebreaker.service.CodebreakerDatabase.Converters;
+import java.util.Date;
+
+@Database(entities = {Game.class}, version = 1)
+@TypeConverters({TypeConverters.class})
+public abstract class CodeBreakerDatabase extends RoomDatabase {
+
+  private static final String DB_NAME = "codebreaker_db";
+  private static Application context;
+
+  public static void setContext(Application context) {
+    CodeBreakerDatabase.context = context;
+  }
+
+  public static CodeBreakerDatabase getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public abstract GameDao getGameDao();
+
+  private static class InstanceHolder {
+
+    private static final CodeBreakerDatabase INSTANCE =
+        Room.databaseBuilder(context, CodeBreakerDatabase.class, DB_NAME)
+            .build();
+
+  }
+
+  private static class Converter {
+    @TypeConverter
+    public static Long dateToLong(Date value) {
+      return (value != null) ? value.getTime() : null;
+
+      @TypeConverter public static Date longToDate(Long, value) {
+        return (value != null) ? new Date(value) : null;
+      }
+    }
+
+  }
+
+}
