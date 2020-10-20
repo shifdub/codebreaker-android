0f23e880baa2f3acbcef5c98d3c523caba239ffa
isaac <shifdub@gmail.com>
2020-10-20 08:51:53 -0600

    after merg with master

--- app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
@@ -2,21 +2,12 @@ package edu.cnm.deepdive.codebreaker;
 
 import android.app.Application;
 import android.util.Log;
-import com.facebook.stetho.Stetho;
-import edu.cnm.deepdive.codebreaker.service.CodeBreakerDatabase;
-import io.reactivex.Scheduler;
-import io.reactivex.schedulers.Schedulers;
 
 public class CodebreakerApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
-    Stetho.initializeWithDefaults(this);
-    CodeBreakerDatabase.setContext(this);
-    CodeBreakerDatabase.getInstance().getGameDao().delete()
-        .subscribeOn(Schedulers.io())
-        .subscribe();
+    Log.d (getClass().getName(),"Testing logging");
   }
 }
-
